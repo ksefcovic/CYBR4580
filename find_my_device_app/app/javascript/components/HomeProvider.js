b5b01e054488f505c1c9efaa7714b13143cf2cfe
Lauren Shultz <larz2016@gmail.com>
2019-09-06 18:22:14 -0500

    setup base rails app with react and postgres database

---
+++ find_my_device_app/app/javascript/components/HomeProvider.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class HomeProvider extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1>This is the home page</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default HomeProvider
