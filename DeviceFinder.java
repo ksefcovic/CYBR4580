9aa59a88c92689f3063d951d0748c120b4268913
Aaron Wurtele <aaronwurtele@gmail.com>
2019-11-12 23:53:11 -0600

    fix all of the compile-time errors w/ post request

--- DeviceFinder.java
+++ DeviceFinder.java
@@ -1,6 +1,7 @@
 package com.android.server;
 
 //get location
+import android.location.Location;
 import android.location.LocationManager;
 //get imei/meid
 import android.telephony.TelephonyManager;
@@ -15,56 +16,60 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class DeviceFinder{
+public class DeviceFinder {
 
-    public void doTheThing(TelephonyManager mTelephonyManager) {
+    public void run(TelephonyManager mTelephonyManager) {
         this.getImei(mTelephonyManager);
         //sendPostRequest(getGPS(), getImei());
     }
 
     //getGPS is a private function called by run() that returns the last known GPS location
+    //latitude and longitude can be accessed using .getLatitude() .getLongitude() on the object returned
     private Location getGPS() {
-        //aa
+        //TODO : change the return value
+        return(null);
     }
 
-    //getImei is a function called by run() that returns the IMEI
+    //getImei is a function called by run() that returns the IMEI as a string
     private String getImei(TelephonyManager mTelephonyManager) {
         String deviceID = mTelephonyManager.getImei();
         //get the IMEI
-        if(deviceID == null) { deviceID = mTelephonyManager.getMeid(); }
+        if (deviceID == null) {
+            deviceID = mTelephonyManager.getMeid();
+        }
         //If the returned value is null, get MEID (apparently the CDMA equivalent??)
-        if(deviceID == null) { deviceID = "9999"; }
+        if (deviceID == null) {
+            deviceID = "9999";
+        }
         //If that fails, set it to 9999 for the sake of testing
         Log.d("ConnectivityService", "AW - " + deviceID);
-        return(deviceID);
+        return (deviceID);
     }
 
     //this function uses the library to send GPS and IMEI to the server
     //it gets passed the GPS location and the imei
     private int sendPostRequest(Location currentLocation, String imei) {
         //latitude and longitude can be accessed using currentLocation.getLatitude() .getLongitude()
-        String url = "http://ec2-3-17-64-157.us-east-2.compute.amazonaws.com/api/v1/check_status";
+        try {
+            String url = "http://ec2-3-17-64-157.us-east-2.compute.amazonaws.com/api/v1/check_status";
 
-        HttpsURLConnection httpClient = (HttpsURLConnection) new URL(url).openConnection();
+            HttpsURLConnection httpClient = (HttpsURLConnection) new URL(url).openConnection();
 
-        httpClient.setRequestMethod("POST");
+            httpClient.setRequestMethod("POST");
 
-        String urlParameters = "imei=9312093821321";
+            String urlParameters = "imei=9312093821321";
 
-        httpClient.setDoOutput(true);
-        try (DataOutputStream wr = new DataOutputStream(httpClient.getOutputStream())) {
+            httpClient.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(httpClient.getOutputStream());
             wr.writeBytes(urlParameters);
             wr.flush();
-        }
 
-        int responseCode = httpClient.getResponseCode();
-        System.out.println("\nSending 'POST' request to URL : " + url);
-        System.out.println("Post parameters : " + urlParameters);
-        System.out.println("Response Code : " + responseCode);
+            int responseCode = httpClient.getResponseCode();
+            Log.d("sendPostRequest", "\nSending 'POST' request to URL : " + url);
+            Log.d("sendPostRequest", "Post parameters : " + urlParameters);
+            Log.d("sendPostRequest", "Response Code : " + responseCode);
 
-        try (BufferedReader in = new BufferedReader(
-                new InputStreamReader(httpClient.getInputStream()))) {
-
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpClient.getInputStream()));
             String line;
             StringBuilder response = new StringBuilder();
 
@@ -73,8 +78,11 @@ public class DeviceFinder{
             }
 
             //print result
-            System.out.println(response.toString());
-
+            Log.d("sendPostRequest", "" + response.toString());
+        } catch (Exception e) {
+            Log.d("sendPostrequest", "Exception: " + e.toString() + " -- " + e.getMessage());
         }
+        //TODO: change the return value
+        return (0);
     }
 }
