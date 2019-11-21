9c2b8221d919cde9ad8cc92ae2140c89f860ad91
Aaron Wurtele <aaronwurtele@gmail.com>
2019-11-20 22:43:32 -0600

    add semaphores (not currently working)

--- DeviceFinder.java
+++ DeviceFinder.java
@@ -12,9 +12,11 @@ import android.os.HandlerThread;
 import android.os.Looper;
 import android.telephony.TelephonyManager;
 //for Log.d
+import android.util.JsonReader;
 import android.util.Log;
 //post request code
 import android.annotation.TargetApi;
+import org.json.JSONObject;
 import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -22,6 +24,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.concurrent.Semaphore;
 
 public class DeviceFinder {
 
@@ -30,11 +33,16 @@ public class DeviceFinder {
     private String lon = null;
     private DeviceFinder df;
     private HandlerThread ht;
+    Semaphore latSem = new Semaphore(1, true);
+    Semaphore lonSem = new Semaphore(1, true);
 
     //setup the location listener
     private LocationListener ll = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
+            //grab the semaphore
+            latSem.acquire();
+            lonSem.acquire();
             //set the locaiton, lat, and lon
             curLocation = location;
             double latitude = curLocation.getLatitude();
@@ -43,6 +51,8 @@ public class DeviceFinder {
             lat = Double.toString(latitude);
             lon = Double.toString(longitude);
             Log.d("DeviceFinder","AW - Lat:"+ lat + " , Long:"+ lon);
+            latSem.release();
+            lonSem.release();
             //kill the handlerthread so we don't continually get updates
             if(ht.quit() == true) { Log.d("DeviceFinder","AW - ht quit"); }
             else Log.d("DeviceFinder","AW - ht quit failed");
@@ -106,21 +116,34 @@ public class DeviceFinder {
             String url = "http://ec2-3-17-64-157.us-east-2.compute.amazonaws.com/api/v1/check_status";
             HttpURLConnection httpClient = (HttpURLConnection) new URL(url).openConnection();
             httpClient.setRequestMethod("POST");
+            httpClient.setRequestProperty("Content-Type", "application/json; charset=utf-8");
             Log.d("sendPostRequest", "\nAW - Sending 'POST' request to URL : " + url);
+
             //put the parameters in a hashmap
             HashMap<String, String> hm = new HashMap();
+
             //hm.put("imei", imei);
-            hm.put("IMEI", "mytestimei640851");
+            hm.put("imei", "mytestimei640851");
+            latSem.acquire();
             hm.put("latitude", lat);
+            latSem.release();
+            lonSem.acquire();
             hm.put("longitude", lon);
+            lonSem.release();
+
             //make it a string to send
-            String data = hm.toString();
+            JSONObject obj = new JSONObject(hm);
+            String data = obj.toString();
+
+            //String data = hm.toString();
             httpClient.setDoOutput(true);
+
             //send the POST Request
             DataOutputStream wr = new DataOutputStream(httpClient.getOutputStream());
             wr.writeBytes(data);
             wr.flush();
             Log.d("sendPostRequest", "AW - Post parameters : " + data);
+
             //get the response code
             int responseCode = httpClient.getResponseCode();
             Log.d("sendPostRequest", "AW - Response Code : " + responseCode);
@@ -138,6 +161,7 @@ public class DeviceFinder {
             //return false to let the driver know it did not work
             return(false);
         }
+
         //return true to let the driver know it worked
         return(true);
     }
